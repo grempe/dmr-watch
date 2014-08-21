e4495d989aa6acf837df9aa11c6089f13eb07304
Glenn Rempe <glenn@rempe.us>
2014-08-20 23:33:39 -0700

    Remove no longer needed Plug static.

--- web/router.ex
+++ web/router.ex
@@ -2,7 +2,6 @@ defmodule DmrWatch.Router do
   use Phoenix.Router
   use Phoenix.Router.Socket, mount: "/ws"
 
-  plug Plug.Static, at: "/static", from: :dmr_watch
   get "/", DmrWatch.PageController, :index, as: :page
 
   channel "netwatch", DmrWatch.NetwatchChannel
