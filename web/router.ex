fe9f1b9a1546b1ec4e580a5d439ab815c0e7e97c
Glenn Rempe <glenn@rempe.us>
2014-08-21 08:52:39 -0700

    Add back static file route.

--- web/router.ex
+++ web/router.ex
@@ -2,6 +2,7 @@ defmodule DmrWatch.Router do
   use Phoenix.Router
   use Phoenix.Router.Socket, mount: "/ws"
 
+  plug Plug.Static, at: "/static", from: :dmr_watch
   get "/", DmrWatch.PageController, :index, as: :page
 
   channel "netwatch", DmrWatch.NetwatchChannel
