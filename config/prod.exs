8d2157ae0111b6fd7b9bdfc378c5d4d5af330d16
Glenn Rempe <glenn@rempe.us>
2014-08-23 08:48:17 -0700

    Update production host name.

--- config/prod.exs
+++ config/prod.exs
@@ -3,7 +3,7 @@ use Mix.Config
 config :phoenix, DmrWatch.Router,
   port: System.get_env("PORT"),
   ssl: false,
-  host: "example.com",
+  host: "dmr-watch.rempe.us",
   cookies: true,
   session_key: "_dmr_watch_key",
   session_secret: "0EE=TT21H0JO99ZRU0W@J9_$&2%HG1GNK(U3YGOE5&#@JE#4)*9$12=3U292)5+P!G$W21HI"
